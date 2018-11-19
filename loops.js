1773bd16e320ac4de0fb34ace0edd8a7731c7df3
FX Wood <fxwood2@gmail.com>
2018-11-19 04:39:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,9 +3,9 @@ var loop = []
 function forLoop(loop)  {
   for (var i = 0; i < 25; i++) {
     if (i<=1) {
-      loop.push(`I am ${i} strange loop`)
+      loop.push(`I am ${i} strange loop.`)
     } else {
-      loop.push(`I am ${i} strange loops`);
+      loop.push(`I am ${i} strange loops.`);
     }
   }
   return loop
