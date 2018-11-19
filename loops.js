5579420a72b98450581c3de6abd0add811a7fbc6
FX Wood <fxwood2@gmail.com>
2018-11-19 05:04:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,13 @@
 var loop = []
 
+function incrementVariable() {
+  i = i + 1;
+}
+
 function forLoop(loop)  {
   for (var i = 0; i < 25; i++) {
     if (i<=1) {
-      loop.push(`I am ${i} strange loop.`)
+      loop.push(`I am ${i} strange loop.`);
     } else {
       loop.push(`I am ${i} strange loops.`);
     }
@@ -13,7 +17,14 @@ function forLoop(loop)  {
 
 function whileLoop(n) {
   while (n > 0) {
-    console.log(--n)
+    console.log(--n);
   }
   return "done"
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+    incrementVariable;
+  } while (array.length > 0 && incrementVariable() === true)
 }
